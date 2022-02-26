398aac7bcbf837890c63cb79bfd0c751351d34f0
bestiegitsit <93808621+bestiegitsit@users.noreply.github.com>
2022-02-25 20:02:23 -0800

    Insert Records into WhatABook

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -35,3 +35,48 @@ CREATE TABLE wishlist (
     FOREIGN KEY(user_id)
         REFERENCES user(user_id)
 );
+
+INSERT INTO store(locale)
+    VALUES('1000 Galvin Rd S, Bellevue, NE 68005');
+
+INSERT INTO book(book_name, author, details)
+    VALUES('The Return of The King', 'J.R.Tolkien', 'The third part of The Lord of The Rings');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Fellowship of The Ring', 'J.R.Tolkien', 'The second part of The Lord of The Rings');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Two Towers', 'J.R.Tolkien', 'The second part of The Lord of The Rings');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Hobbit or There and Back Again', 'J.R.Tolkien');
+INSERT INTO book(book_name, author, details)
+    VALUES('Dune: Deluxe Edition', 'Frank Herbert');
+INSERT INTO book(book_name, author, details)
+    VALUES("Charlotee's Web", 'E.B. White');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Great Gatsby', 'F. Scott Fitzgerald');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Lion, The Witch, and The Wardrobe', 'C.S. Lewis');
+INSERT INTO book(book_name, author, details)
+    VALUES('The Catcher and The Rye', 'J.D. Salinger');
+
+INSERT INTO user(first_name, last_name)
+    VALUES('Thorin', 'Oakenshield');
+INSERT INTO user(first_name, last_name)
+    VALUES('Bilbo', 'Bagging');
+INSERT INTO user(first_name, last_name)
+    VALUES('Frodo', "Baggins');
+
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Thorin'),
+        (SELECT book_id FROM book WHERE book_name = 'The Hobbit or There and Back Again')
+    );
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Bilbo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Fellowship of The Ring')
+    );
+INSERT INTO wishlist(user_id, book_id)
+    VALUES (
+        (SELECT user_id FROM user WHERE first_name = 'Frodo'),
+        (SELECT book_id FROM book WHERE book_name = 'The Return of The King')
+    );
