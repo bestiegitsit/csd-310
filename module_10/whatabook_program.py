45e8dca4e3228ec5ba0a1934ba69e29fe2f6faff
bestiegitsit <93808621+bestiegitsit@users.noreply.github.com>
2022-02-28 22:48:30 -0800

    edit phyton syntax

--- module_10/whatabook_program.py
+++ module_10/whatabook_program.py
@@ -57,7 +57,7 @@ def validate_user():  # user_id validation
 def show_account_menu():  # select user menu
     try:
         print("\n      -- Customer Menu --")
-        print("        1. Wishlist\n         2. Add Book\n        3. Main Menu")
+        print("        1. Wishlist\n        2. Add Book\n        3. Main Menu")
         account_option = int(input("        <Example enter: 1 for wishlist>: "))
         return account_option
     except ValueError:
@@ -65,20 +65,20 @@ def show_account_menu():  # select user menu
         sys.exit(0)
 
 def show_wishlist(_cursor, _user_id):  # select a users wishlist to display
-    _cursor.execute("SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author " +
-    "FROM wishlist " +
-    "INNER JOIN user ON wishlist.user_id = user.user_id " +
-    "INNER JOIN book ON wishlist.book_id = book.book_id " +
-    "WHERE user.user_id = {}".format(_user_id))
+    _cursor.execute('SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author ' +
+    'FROM wishlist ' +
+    'INNER JOIN user ON wishlist.user_id = user.user_id ' +
+    'INNER JOIN book ON wishlist.book_id = book.book_id ' +
+    'WHERE user.user_id = {}'.format(_user_id))
     wishlist = _cursor.fetchall()
     print("\n        -- DISPLAYING WISHLIST ITEMS --")
     for book in wishlist:
         print("        Book Name: {}\n        Author: {}\n".format(book[4], book[5]))
 
 def show_books_to_add(_cursor, _user_id):  # show books not listed in the wishlist
-    query = ("SELECT book_id, book_name, author, details "
-            "FROM book "
-            "WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = {})".format(_user_id))
+    query = ('SELECT book_id, book_name, author, details '
+            'FROM book '
+            'WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = {})'.format(_user_id))
     print(query)
     _cursor.execute(query)
     books_to_add = _cursor.fetchall()
@@ -87,7 +87,7 @@ def show_books_to_add(_cursor, _user_id):  # show books not listed in the wishli
         print("        Book Id: {}\n        Book Name: {}\n".format(book[0], book[1]))
 
 def add_book_to_wishlist(_cursor, _user_id, _book_id):  # select a book to add to a wishlist
-    _cursor.execute("INSERT INTO wishlist(user_id, book_id) VALUES({}, {})".format(_user_id, _book_id))
+    _cursor.execute('INSERT INTO wishlist(user_id, book_id) VALUES({}, {})'.format(_user_id, _book_id))
 
 try:  # handling potential errors
     db = mysql.connector.connect(**config) 
