c6c16f417a6126e98b722f97089fe7c9238f1066
bestiegitsit <93808621+bestiegitsit@users.noreply.github.com>
2022-02-28 18:31:13 -0800

    Finalizing INIT

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -45,17 +45,17 @@ INSERT INTO book(book_name, author, details)
     VALUES('The Fellowship of The Ring', 'J.R.Tolkien', 'The second part of The Lord of The Rings');
 INSERT INTO book(book_name, author, details)
     VALUES('The Two Towers', 'J.R.Tolkien', 'The second part of The Lord of The Rings');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES('The Hobbit or There and Back Again', 'J.R.Tolkien');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES('Dune: Deluxe Edition', 'Frank Herbert');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES("Charlotee's Web", 'E.B. White');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES('The Great Gatsby', 'F. Scott Fitzgerald');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES('The Lion, The Witch, and The Wardrobe', 'C.S. Lewis');
-INSERT INTO book(book_name, author, details)
+INSERT INTO book(book_name, author)
     VALUES('The Catcher and The Rye', 'J.D. Salinger');
 
 INSERT INTO user(first_name, last_name)
