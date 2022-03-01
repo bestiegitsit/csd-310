744e34481c1d8989ea87876167073e75d1292fad
bestiegitsit <93808621+bestiegitsit@users.noreply.github.com>
2022-02-28 17:16:12 -0800

    Test update

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -1,3 +1,7 @@
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
+CREATE USER 'whatabook_user'@'localhost';
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
 ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
 ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
 DROP TABLE IF EXISTS store;
@@ -6,34 +10,30 @@ DROP TABLE IF EXISTS wishlist;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE store (
-    store_id    INT     NOT NULL    AUTO_INCREMENT,
+    store_id INT NOT NULL AUTO_INCREMENT,
     locale      VARCHAR(500)    NOT NULL,
     PRIMARY KEY(store_id)
 );
 CREATE TABLE book (
-    book_id INT NOT NULL    AUTO_INCREMENT,
+    book_id INT NOT NULL AUTO_INCREMENT,
     book_name   VARCHAR(200)    NOT NULL,
     author  VARCHAR(200)    NOT NULL,
     details VARCHAR(500),
     PRIMARY KEY(book_id)
 );
 CREATE TABLE user (
-    user_id INT NOT NULL    AUTO_INCREMENT,
+    user_id INT NOT NULL AUTO_INCREMENT,
     first_name  VARCHAR(75) NOT NULL,
     last_name   VARCHAR(75) NOT NULL,
     PRIMARY KEY(user_id)
 );
 CREATE TABLE wishlist (
-    wishlist_id INT NOT NULL    AUTO_INCREMENT,
+    wishlist_id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     book_id INT NOT NULL,
-    PRIMARY KEY(wishlist_id),
-    CONSTRAINT fk_book
-    FOREIGN KEY(book_id)
-        REFERENCES book(book_id),
-    CONSTRAINT fk_user
-    FOREIGN KEY(user_id)
-        REFERENCES user(user_id)
+    PRIMARY KEY (wishlist_id),
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES book(book_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 INSERT INTO store(locale)
